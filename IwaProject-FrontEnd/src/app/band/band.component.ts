5cfc5b46dc235f5d2665eb5ee42600c33cb16ce3
Darcam56 <lilgayet@gmail.com>
2020-06-08 12:51:10 +0200

    Add Band component

---
+++ IwaProject-FrontEnd/src/app/band/band.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-band',
+  templateUrl: './band.component.html',
+  styleUrls: ['./band.component.css']
+})
+export class BandComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
