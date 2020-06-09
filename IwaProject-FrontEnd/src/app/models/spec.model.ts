28c7153de78cbe41e11f31b1b74f230f5508abde
Darcam56 <lilgayet@gmail.com>
2020-06-09 20:19:11 +0200

    Spectator Model

---
+++ IwaProject-FrontEnd/src/app/models/spec.model.ts
@@ -0,0 +1,11 @@
+export class Spectator{
+  id: number;
+  firstname: string;
+  lastname: string;
+
+  constructor(id: number, firstname: string, lastname: string) {
+    this.id =  id;
+    this.firstname = firstname;
+    this.lastname = lastname;
+  }
+}
