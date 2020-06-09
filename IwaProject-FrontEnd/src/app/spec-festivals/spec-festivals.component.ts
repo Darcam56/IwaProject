e6a479964aef95b2579f2df22eb1b95eb9202bf8
Darcam56 <lilgayet@gmail.com>
2020-06-09 23:11:55 +0200

    Add filter to fesList

--- IwaProject-FrontEnd/src/app/spec-festivals/spec-festivals.component.ts
+++ IwaProject-FrontEnd/src/app/spec-festivals/spec-festivals.component.ts
@@ -3,6 +3,7 @@ import {TokenStorageService} from '../auth/token-storage.service';
 import {Festival} from '../models/festival.model';
 import {SpecService} from '../services/spec.service';
 import {FestivalService} from '../services/festival.service';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-spec-festivals',
@@ -13,6 +14,7 @@ export class SpecFestivalsComponent implements OnInit {
 
   festList: Festival[];
   displayedColumns: string[] = ['Name', 'Descritpion', 'Order'];
+  dataSource: MatTableDataSource<any>;
 
   constructor(private festivalService: FestivalService,
               private spectatorService: SpecService,
@@ -22,10 +24,19 @@ export class SpecFestivalsComponent implements OnInit {
     this.getFestivals();
   }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    this.dataSource.filterPredicate = (fest: Festival, filter: string) => {
+      return fest.festivalName.toLowerCase().includes(filter);
+    };
+  }
+
   getFestivals(): void{
     this.festivalService.getFestivals()
       .subscribe(festList => {
         this.festList = festList;
+        this.dataSource = new MatTableDataSource(this.festList);
         console.log(this.festList);
       });
   }
