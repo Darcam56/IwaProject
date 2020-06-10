1e1f665c402b32c8369e35da00220a6ed19bff77
Darcam56 <lilgayet@gmail.com>
2020-06-10 18:06:05 +0200

    Org service

--- IwaProject-FrontEnd/src/app/models/stage.model.ts
+++ IwaProject-FrontEnd/src/app/models/stage.model.ts
@@ -1,11 +1,6 @@
-import {Concert} from './concert.model';
-
 export class Stage{
   id: number;
   name: string;
-  concerts: Concert[];
-
-  stage: Stage;
 
   constructor(id: number, name: string) {
     this.id =  id;
---
+++ IwaProject-FrontEnd/src/app/services/org.service.ts
@@ -0,0 +1,94 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable, of} from 'rxjs';
+import {catchError, tap} from 'rxjs/operators';
+import {Organiser} from '../models/organiser.model';
+import {Stage} from '../models/stage.model';
+import {Festival} from '../models/festival.model';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-type': 'application/json'})
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrganiserService {
+
+  private festsUrl = 'http://localhost:8080/organisers';
+
+  constructor(private http: HttpClient) {
+  }
+
+  /** GET: get organisers from the server  */
+  getOrganisers(): Observable<Organiser[]> {
+    return this.http.get<Organiser[]>(this.festsUrl);
+  }
+
+  /** GET: get organiser by id. 404 if not found */
+  getOrganiser(id: number): Observable<Organiser> {
+    const url = `${this.festsUrl}/${id}`;
+    return this.http.get<Organiser>(url).pipe(
+      tap(_ => this.log(`fetched organiser id=${id}`)),
+      catchError(this.handleError<Organiser>(`getOrganiser id=${id}`))
+    );
+  }
+
+  /** GET: get Stages Organiser by id. 404 if not found */
+  getFestivals(username: string): Observable<Festival[]> {
+    const url = `${this.festsUrl}/${username}/festivals`;
+    return this.http.get<Festival[]>(url).pipe(
+      tap(_ => this.log(`fetched organisers fests id=${username}`)),
+      catchError(this.handleError<Festival[]>(`getFestivals id=${username}`))
+    );
+  }
+
+  /** POST: add a new organiser to the server */
+  addFestivalToOrganiser(username: string, stage: Stage): Observable<Festival> {
+    const url = `${this.festsUrl}/${username}/festivals`;
+    return this.http.post<Festival>(url, stage, httpOptions).pipe(
+      tap((festivalAdded: Festival) => this.log(`added stage id=${festivalAdded.id}`)),
+      catchError(this.handleError<Festival>(`addFestivalToOrganiser`))
+    );
+  }
+
+  /** DELETE: delete the organiser from the server */
+  deleteOrganiser(organiser: Organiser | number): Observable<Organiser> {
+    const id = typeof organiser === 'number' ? organiser : organiser.id;
+    const url = `${this.festsUrl}/${id}`;
+
+    return this.http.delete<Organiser>(url, httpOptions).pipe(
+      tap(_ => this.log(`deleted organiser id=${id}`)),
+      catchError(this.handleError<Organiser>(`deleteOrganiser`))
+    );
+  }
+
+  /** DELETE : Delete all the organisers */
+  deleteAllOrganisers(): Observable<any> {
+    return this.http.delete(this.festsUrl, httpOptions).pipe(
+      tap(_ => this.log(`deleted organisers`)),
+      catchError(this.handleError<any>(`deleteAllOrganiser`))
+    );
+  }
+
+  /**
+   * Handle Http operation that failed
+   * Let the app continue
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+
+      this.log(`${operation} failed: ${error.message}`);
+
+      return of(result as T);
+    };
+  }
+
+  /** Log a OrganiserService message with the message service */
+  private log(message: string) {
+    console.log('OrganiserService: ' + message);
+  }
+}
